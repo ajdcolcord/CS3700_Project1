88d90993e54aadf87bcf4c6821735ed85d854336
Austin Colcord <ajdcolcord@gmail.com>
2016-01-18 18:18:55 -0500

    attempting to resolve issues with receiving messages... still in progress

--- client.py
+++ client.py
@@ -8,28 +8,52 @@ sflag = str(sys.argv[2])
 hostname = str(sys.argv[3])
 neuid = str(sys.argv[4])
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 host = socket.gethostname()
 
 ipaddr = socket.gethostbyname(hostname)
 
-print ipaddr
-
-sock.connect((ipaddr, int(port)))
-
-send_message('HELLO' + neuid)
-
-message = sock.recv(1024)
-
+def get_message():
+    mess = sock.recv(1024)
+    parse_message(mess)
+    
 def parse_message(message):
-	message_arr = str(message).split()
-	if message_arr[0] == 'STATUS':
-		result = message_arr[1] #CHANGE THIS
-		#parse the function for 1, 2, 3 
-		send_message(result)
-	elif message_arr[0] == 'BYE':
-		print message_arr[1] + '\n'
-		sock.close()
+    message_arr = str(message).split()
+    if message_arr[0] == 'STATUS':
+        problem = message_arr[1] #CHANGE THIS
+        print result
+        result = ""
+        send_message(result)
+    elif message_arr[0] == 'BYE':
+        print message_arr[1] + '\n'
+        sock.close()
+    else:
+        print "NO MESSAGE"
 
 def send_message(message):
-	sock.send(message + '\n');
+    sock.send(message + '\n');
+
+print ipaddr
+
+try:
+    sock.connect((ipaddr, int(port)))
+except:
+    alert("FAILED TO CONNECT")
+
+#send hello
+try:
+    send_message('HELLO' + neuid)
+except:
+    "Failed to send HELLO"
+#get initial message
+message = True
+
+while message:
+    print message
+    message = sock.recv(1024)
+    if message:
+        print message
+        parse_message(message)
+
+sock.close()
